0973e4bb09fa13ad1d446db279763dc8446daa82
Joao Pedro <whitenoob5@gmail.com>
2024-12-12 14:24:54 -0300

    Continuando a responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,10 @@
-let imagens=[
-    './src/assets/imagens/banner/persona3.png',
-    './src/assets/imagens/banner/undertale.png',
-    './src/assets/imagens/banner/ds3.webp',
-]
-
-let index =0;
-let time =3000;
-
-function slideShow(){
-    document.getElementById("banner").src=imagens[index];
-    index++;
-    if(index == imagens.length){ index =0}
-    setTimeout("slideShow()",time)
+let menu = document.getElementsByClassName("menu")[0];
+console.log(menu)
+function ativar(){
+    menu.classList.add('ativo');
+    console.log("a")
 }
-
-slideShow();
+function desativar(){
+    menu.classList.remove('ativo');
+    console.log("b")
+}
